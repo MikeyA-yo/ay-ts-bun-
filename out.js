26d6703dcd35a237599918cbb5665a7db0842592
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-04-26 19:34:46 +0100

    i'm worried about deno

--- out.js
+++ out.js
@@ -2,30 +2,9 @@
  import { rand, round, PI, floor, exp, degToRad, radToDeg } from './math';
  import { print, timer, Day, interval, read, write, appendFile, dirname } from './utils';
   try {
-let a = "my program variables are nicely scoped" ; 
-console.log(a)   ; 
-let b = "hello world" ; 
-let c = 3 + 3 ; 
-let d = round(rand() * 12) ; 
-function print5(){ 
-for ( let i = 0; i < 5; i++ ) { 
-let b2 = round(rand() * 5) ; 
-if ( d > c ) { 
-console.log(b)   ; 
-}else{ 
-print(b2) ; 
-} 
-} 
-} 
-print5() ; 
-let today = new Day() ; 
-let time = today.time() ; 
-//l data = read('semi.js') ; 
-//write('new.js', data) 
-print(`${today.getFullDate()} - ${time}`) 
-const {bugs} = require("./out2.js")    ; 
-//print data ; 
-bugs() ; 
+timer(()=>{ console.log(5)   }, 500) 
+const {d} = require("./out2.js")    ; 
+// this imports and execute a file ; 
 }catch(e){
  console.error(e.message);
 }
--- out2.js
+++ out2.js
@@ -2,12 +2,13 @@
  const {print, timer, Day, interval, read, write, appendFile, dirname} = require('./utils')
  const {AY} = require(__dirname +'/objects/AY');
   try {
- let ay = 'Peak' ; 
- function bugs ( ) { 
-console.log(ay)   ; 
+for ( let i = 0; i < 20; i++ ) { 
+i % 3 == 0 ? console.log('fizz')   : i % 5 == 0 ? console.log('buzz')   : console.log(i)   ; 
 } 
-; 
-module.exports = {ay,bugs}
+print(dirname(), AY.type(AY.isAy)) 
+console.log(AY.os)   ; 
+ let d = AY.os ; 
+module.exports = {d}
 }catch(e){
  console.error(e.message);
 }
