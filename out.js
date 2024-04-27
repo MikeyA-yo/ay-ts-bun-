c6a61f4222a95564d6f735ed49a359e8d5eccc35
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-04-27 22:07:48 +0100

    idk man lol

--- out.js
+++ out.js
@@ -2,9 +2,12 @@
  import { rand, round, PI, floor, exp, degToRad, radToDeg } from './math';
  import { print, timer, Day, interval, read, write, appendFile, dirname } from './utils';
   try {
-timer(()=>{ console.log(5)   }, 500) 
-const {d} = require("./out2.js")    ; 
-// this imports and execute a file ; 
+for ( let i = 0; i < 20; i++ ) { 
+i % 3 == 0 ? console.log('fizz')   : i % 5 == 0 ? console.log('buzz')   : console.log(i)   ; 
+} 
+print(dirname(), AY.type(AY), AY.type(dirname), AY.os, AY.argv[1]) 
+const {ay} = require("./out2.js")    ; 
+console.log(ay)   ; 
 }catch(e){
  console.error(e.message);
 }
--- out2.js
+++ out2.js
@@ -2,13 +2,12 @@
  const {print, timer, Day, interval, read, write, appendFile, dirname} = require('./utils')
  const {AY} = require(__dirname +'/objects/AY');
   try {
-for ( let i = 0; i < 20; i++ ) { 
-i % 3 == 0 ? console.log('fizz')   : i % 5 == 0 ? console.log('buzz')   : console.log(i)   ; 
+for ( let i = 0; i < 10000; i++ ) { 
+console.log(i)   ; 
 } 
-print(dirname(), AY.type(AY.isAy)) 
-console.log(AY.os)   ; 
- let d = AY.os ; 
-module.exports = {d}
+ let data = 'imports n exports' ; 
+ let ay = 'me' ; 
+module.exports = {data,ay}
 }catch(e){
  console.error(e.message);
 }
